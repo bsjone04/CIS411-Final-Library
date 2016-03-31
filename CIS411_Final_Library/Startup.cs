fe88a2b6e52534e7685fb490d52de70bc60a06d9
ebdecker <ebdeck01@cardmail.louisville.edu>
2016-03-31 15:19:08 -0400

    initial commit

---
+++ CIS411_Final_Library/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CIS411_Final_Library.Startup))]
+namespace CIS411_Final_Library
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
